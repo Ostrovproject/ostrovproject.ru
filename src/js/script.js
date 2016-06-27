66a95b0a979809f47f217033a8f35c2e4a1f8c9d
Alexander Mospan <alexander.mospan@gmail.com>
2016-06-27 14:52:09 +0300

    beta 1 with form

--- src/js/script.js
+++ src/js/script.js
@@ -7,17 +7,23 @@ $(document).ready(function(){
 
 
 
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-  
+  // $('a[href*=#]:not([href=#])').click(function() {
+  //   if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+  //     var target = $(this.hash);
+  //     target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+  //     if (target.length) {
+  //       $('html,body').animate({
+  //         scrollTop: target.offset().top
+  //       }, 1000);
+  //       return false;
+  //     }
+  //   }
+  // });
+
+  $('.j-request').on('click', function () {
+    require(["mojo/signup-forms/Loader"], function (L) {
+      L.start({"baseUrl": "mc.us13.list-manage.com", "uuid": "b72d9b54f584b4b190d5fc5f5", "lid": "e3096ec456"})
+    });
+  })
+
 });
